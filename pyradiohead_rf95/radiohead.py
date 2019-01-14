b6a033778b7302c401d2b0681a7d8502e9fe726b
Nicolas CELLIER <celliern@users.noreply.github.com>
2019-01-14 15:48:43 +0100

    Update radiohead.py

--- pyradiohead_rf95/radiohead.py
+++ pyradiohead_rf95/radiohead.py
@@ -40,7 +40,7 @@ class RF95:
 
     def init(self):
         r = radiohead.init()
-        if r != 0:
+        if r != 1:
             raise RuntimeError("RF95 init failed - value: %i" % r)
 
     def set_tx_power(self, power, useRFO):
